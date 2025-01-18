fe1d1da1d7e79bc8468a2760290db3c6f675341d
Incoronato Eleonora <incoronatodesign@gmail.com>
2025-01-18 01:52:03 +0100

    abbiamo cambiato icodici di nuovo
    abbiamo fatto grossi cambiamenti

--- presentazione finale/publication-web-resources/script/main.js
+++ presentazione finale/publication-web-resources/script/main.js
@@ -1,40 +1,56 @@
-var currentPage = 0;
-const totalHtmlFiles = 10;
-function changePublication() {
-	if (currentPage >= 0 && currentPage < totalHtmlFiles) {
-		var currentPageUrl = document.getElementById("contentIFrame").src;
-		currentPageUrl = currentPageUrl.substring(0, currentPageUrl.lastIndexOf("/") + 1);
-		var nextPageUrl = currentPageUrl;
-		if (currentPage !== 0)
-			currentPageUrl = currentPageUrl + "publication-" + currentPage + ".html";
-		else
-			currentPageUrl = currentPageUrl + "publication" + ".html";
-		document.getElementById("contentIFrame").src = currentPageUrl;
-		if ((currentPage + 1) < totalHtmlFiles) {
-			nextPageUrl = nextPageUrl + "publication-" + (currentPage + 1) + ".html";
-			document.getElementById("dummyIFrame").src = nextPageUrl;
-		}
-	}
-}
-function showNextPage() {
-	++currentPage;
-	changePublication();
-	showHideArrows();
-}
-function showPreviousPage() {
-	--currentPage;
-	changePublication();
-	showHideArrows();
-}
-function showHideArrows() {
-	if (currentPage === 0) {
-		document.getElementsByClassName("prev")[0].style.visibility = "hidden";
-	} else {
-		document.getElementsByClassName("prev")[0].style.visibility = "visible";
-	}
-	if (currentPage === (totalHtmlFiles -1)) {
-		document.getElementsByClassName("next")[0].style.visibility = "hidden";
-	} else {
-		document.getElementsByClassName("next")[0].style.visibility = "visible";
-	}
-}
+// JavaScript Ottimizzato per navigazione e funzionalità (main.js)
+
+document.addEventListener('DOMContentLoaded', function () {
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const iframe = document.querySelector('#contentIFrame');
+
+    // Array delle URL delle pagine per navigazione
+    const pages = [
+        "publication-1.html",
+        "publication-2.html",
+        "publication-3.html",
+        "publication-4.html",
+        "publication-5.html",
+        "publication-6.html",
+        "publication-7.html",
+        "publication-8.html",
+        "publication-9.html"
+    ];
+
+    let currentPageIndex = 0;
+
+    // Funzione per aggiornare il contenuto dell'iframe
+    function updateIframe(index) {
+        if (index >= 0 && index < pages.length) {
+            iframe.src = `publication-web-resources/html/${pages[index]}`;
+        }
+    }
+
+    // Gestione click pulsanti
+    prevButton.addEventListener('click', function () {
+        if (currentPageIndex > 0) {
+            currentPageIndex--;
+            updateIframe(currentPageIndex);
+        }
+    });
+
+    nextButton.addEventListener('click', function () {
+        if (currentPageIndex < pages.length - 1) {
+            currentPageIndex++;
+            updateIframe(currentPageIndex);
+        }
+    });
+
+    // Inizializzazione
+    updateIframe(currentPageIndex);
+
+    // Mostra/nascondi frecce
+    function toggleArrows() {
+        prevButton.style.display = currentPageIndex === 0 ? 'none' : 'block';
+        nextButton.style.display = currentPageIndex === pages.length - 1 ? 'none' : 'block';
+    }
+
+    iframe.addEventListener('load', toggleArrows);
+    toggleArrows();
+});
